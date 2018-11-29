2afaa6527746a628878b0df0594d3f593f6b5d86
Kevin Decock <kevindec@gmail.com>
2018-11-29 17:35:18 +0100

    add converter CLI

--- bin/index.js
+++ bin/index.js
@@ -7,36 +7,27 @@ const path = require("path");
 
 program
 	.version("0.0.1")
-	.option("-r, --readme", "Add readme")
+	.option("-c, --convert [input]", "Convert model to dae [model]", "model")
 	.option("-e, --eslint", "Add eslint config")
 	.parse(process.argv);
 
 
-/**
- * todo: add webpack analyser
- * todo: add .gitignore
- */
+if (program.convert) {
+	const input = program.convert;
+	const inputWithoutExtension = input.split(".")[0];
 
+	console.log("converting your input model to a .dae file");
+	// convert the model to something we can work with
+	shell.exec(`assimp export ${input} ${inputWithoutExtension}.dae`);
 
-
-if (program.readme) {
-	console.log("Adding the SHD default readme template");
-	const readmePath = path.resolve(__dirname, "README.md");
-
-	fs.copyFile(readmePath, "./README.md", (err) => {
-		if (err) {
-			throw err;
-		}
-		console.log("successfully added README.md template");
-
-    /**
-     * todo -> find / replace things like authors, project name, npm scripts....
-     */
-	});
+	// if the model was created on windows, swap out all the backslashes with foreward slashes for unix use
+	const script = `sed -i "" 's.\\\\./.g' ${inputWithoutExtension}.dae`;
+	shell.exec(script);
 }
 
+
 if (program.eslint) {
-	console.log("Adding the SHD default ESLINT configuration");
+	console.log("Adding our ESLINT configuration");
 
 	shell.exec("npm i --save-dev eslint @studiohyperdrive/eslint-config");
 	const eslintPath = path.resolve(__dirname, "ESLINT.js");
